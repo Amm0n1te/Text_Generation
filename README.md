53f750a26294b8e81e5c274f31a61a749d5293b2
Casey <caxchen@ucsc.com>
2022-06-22 20:34:31 -0700

    added README and other minor changes

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# p1-text-generation-starter
+TITLE: Space Commander Text Generator
 
-Credit: Adam Smith
+ARTIST STATEMENT
+My objective was to create a text generator that would generate interesting unexpected results based on a framework of a spaceship commander
+being assigned to investigate an anomaly in some fictional area of space.  I think I have achieved this effect.
+
+EXTRA LAYER OF COMPLEXITY
+script.js line 20: One of the array elements is "a Graal$post $vessel".  It replaces $post and $vessel.
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const fillers = {
     steve: ["Steve", "Dave", "Coreaux", "Dude", "Calestar", "Shephard", "AAAAAAAAAA", "Ikari", "Vost", "Argo", "Felstar", "Sahalin", "Toby Maquire from Spiderman 2 (2004) released on DVD and Blu-Ray", "Zora", "Blackwood"],
     decree: ["royal decree", "vote", "the orders", "consensus", "the wisdom", "their holy light"],
-    supreme: ["Supreme", "Grand", "God", "Green", "Red", "Humble", "Lord"],
+    supreme: ["Supreme", "Grand", "God", "Green", "Red", "Humble", "Lord", "Didactic", "Dudely"],
     chancellor: ["Chancellor", "Emperor", "King", "Queen", "Empress", "Inquisitor", "President", "Chairman", "Leader", "Dude", "Overseer", "Inquisitor", "Executive Council"],
     galactic: ["Galactic", "Planetary", "Ekumenary", "Grand", "Holy", "Interstellar", "United"],
     federation: ["Federation", "Empire", "Republic", "Kingdom", "House of $steve", "Hegemony"],
+    new: ["state-of-the-art", "brand new", "venerable", "old", "shitty", "advanced", "cutting edge", "powerful", "great", "amazing"],
     vessel: ["frigate", "cruiser", "battle cruiser", "dreadnought", "scoutship", "torchship", "spinship", "starship", "asteroid-cruiser", "flagship"],
-    iron: ["Iron", "Crystal", "Steel", "Blue", "Holy", "Fane", "Heavenly", "Unholy", "Lonely", "Polar", "Dark", "Mystic", "Arctic", "Black"], 
+    iron: ["Iron", "Crystal", "Steel", "Blue", "Holy", "Fane", "Heavenly", "Unholy", "Lonely", "Polar", "Dark", "Mystic", "Arctic", "Black", "Dudely"], 
     serpent: ["Serpent", "Spear", "Sword", "Cross", "Fire", "Spirit", "Dude", "Dragon", "Star", "Vision", "Sun"],
     anomadj: ["strange", "really wierd", "unexplained", "odd", "crazy"],
     anomalies: ["anomalies", "events", "disappearances", "sensor blips", "alien attacks", "chemical phenomena", "stuff", "seismic anomalies"],
@@ -16,8 +17,8 @@ const fillers = {
     post: ["fera", "el", "an", "chak", "urz", "min", "kren", "shi", "delar", "mun", "ana", "venna", "telios", "elia", "a", "nuts", "sandor"], 
     clandestine: ["Subversives", "Krisk", "Investigations", "Anomalies", "Unexplained Phenomena", "Helios", "Military Intelligence", "Special Tactics"],
     group: ["Group", "Task Force", "Agency", "Bureau"],
-    pirates: ["pirates", "evil space Nazis", "Ota$post Raiders", "Graal$post Reavers", "kamikaze space chickens", "berzerk AIs", "a chicken", "beans", "GEESE", "\"bees\"", "an enemy $vessel"],
-    godspeed: ["Godspeed", "Have a nice day", "Good luck", "Cheers", "Be careful out there", "May the force be with you", "Eat your veggies"]
+    pirates: ["space pirates", "evil space Nazis", "Ota$post Raiders", "a Graal$post $vessel", "kamikaze space chickens", "berzerk AIs", "a chicken", "beans", "GEESE", "\"bees\"", "an enemy $vessel"],
+    godspeed: ["Godspeed", "Have a nice day", "Good luck", "Cheers", "Be careful out there", "May the force be with you", "Eat your veggies", "bruh,"]
     
   };
   
@@ -30,7 +31,7 @@ const fillers = {
 
   
   const template = `Commander $steve, 
-  by $decree of the $supreme $chancellor of the $galactic $federation, you shall be given command of the state-of-the-art $vessel 
+  By $decree of the $supreme $chancellor of the $galactic $federation, you shall be given command of the $new $vessel 
   the $iron $serpent.  Your first mission will to be to investigate reports of $anomadj $anomalies in the $outer $rim.  Agent $pre$post of the 
   $clandestine $group will accompany you as an observer.  Be advised, here have been reports of $pirates in the area.
 
